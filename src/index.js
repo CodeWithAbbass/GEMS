8670bf6e00c0a3538b08bbbfdd9128fd8758e465
Abbas Ali <Abbas152052@gmail.com>
2022-11-24 16:10:21 +0500

    #

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppProvider } from "./context/product_context";
-import { FilterContextProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppProvider>
-    <FilterContextProvider>
       <CartProvider>
         <App />
       </CartProvider>
-    </FilterContextProvider>
   </AppProvider>
 );
 
